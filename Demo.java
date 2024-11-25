6c725a28ab21f4020a0350326a6132e7e72e6f36
prabul123 <85386688+prabul123@users.noreply.github.com>
2024-11-25 22:37:31 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -6,14 +6,9 @@ class Demo
 	public static void main(String[] args{
 		System.out.println(a+b);
 	}
-<<<<<<< HEAD
-	sysout(hello);
-
-
-=======
+	
 	int a=10;
 	int b=20;
 	int c=30;
 	int d=40;
->>>>>>> c502911ea1b5b01d2b5cd4aa6b518eb2efbe191e
 }
